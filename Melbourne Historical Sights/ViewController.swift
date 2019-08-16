8b6834a6c438307798a31bcb4e3bee8302087740
ianestraikh <y.estraikh@gmail.com>
2019-08-16 15:15:46 +1000

    First commit

---
+++ Melbourne Historical Sights/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Melbourne Historical Sights
+//
+//  Created by Ian Estraikh on 16/8/19.
+//  Copyright © 2019 Ian Estraikh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
